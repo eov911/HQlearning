c661c3a08dd330c7ba0d8d6952c7b17210965d73
Оксана Ерегина <oksanaeregina@gmail.com>
2023-10-03 16:00:07 +0300

    модель course

--- HQlearning/courses/models.py
+++ HQlearning/courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Course(models.Model):
+    name = models.TextField()
+    owner = models.ForeignKey(User,
+                              on_delete=models.CASCADE,
+                              related_name='courses')
+    
