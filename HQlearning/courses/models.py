f921e36411293ae3852410e5db2143d97660c042
Оксана Ерегина <oksanaeregina@gmail.com>
2023-10-06 06:23:24 +0300

    Добавлена модель Lesson

--- HQlearning/courses/models.py
+++ HQlearning/courses/models.py
@@ -9,4 +9,23 @@ class Course(models.Model):
     owner = models.ForeignKey(User,
                               on_delete=models.CASCADE,
                               related_name='courses')
-    
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    name = models.TextField()
+    link = models.TextField()
+    duration = models.PositiveBigIntegerField()
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+
+    def __str__(self):
+        return self.name
